76a0cf511c4c1e898ba9be17c6e75400931e490e
Pietari Poutiainen <pietari.poutiainen@gmail.com>
2021-05-01 23:51:10 +0300

    Added a common requests session in commons with User-Agent configured

--- commons.py
+++ commons.py
@@ -1,6 +1,16 @@
-
+import requests
 
 class Commons:
 
     _OSRS_API = "http://services.runescape.com/m=itemdb_oldschool/api"
     _OSRS_WIKI_API = "https://prices.runescape.wiki/api/v1/osrs/"
+
+    _SESSION = requests.Session()
+    _SESSION.headers = {
+        'User-Agent': 'GETrackApp/0.1',
+        'From': 'pietari.poutiainen@gmail.com'
+    }
+
+    @staticmethod
+    def session():
+        return Commons._SESSION
