26c48631132e30073fa4179ecc66b888bd01fdfc
Pietari Poutiainen <pietari.poutiainen@gmail.com>
2021-05-01 23:52:39 +0300

    Added entry 2 to journal that fetches demo data from the OSRS Wiki API with /mapping. Prepared text for entry3 task

--- getrecked.py
+++ getrecked.py
@@ -1,70 +1,50 @@
 from getrecked import app
-from flask import jsonify
-
-from flask import render_template
+from flask import jsonify, render_template
+from .commons import Commons
+import time
+from datetime import datetime
 
 @app.route('/')
 def root():
 
     labels, values = make_data_for_demograph()
 
-    return render_template('index.html', labels=labels[:], values=values[:])
 
-from .commons import Commons
-import requests
-
-@app.route('/apidemo')
-def apidemo():
-
-    sample_url= "{}/{}".format(Commons._OSRS_API, 
-        "catalogue/detail.json?item=50")
-    response = requests.get(sample_url).json()
-    return jsonify(response)
-
-import time
-
-def current_milli_time():
-    return round(time.time() * 1000)
-
-from datetime import datetime
-
-"""
-    Need to Setup this User Agent for RuneWiki Real-time pricing apph
-meseries?id=4151
-
-ehrmagerd this data is good!:
-https://prices.runescape.wiki/api/v1/osrs/timeseries?id=4151&timestep=5m
-"""
-
-session = requests.Session()
-session.headers = {
-    'User-Agent': 'APITesting/1.0',
-    'From': 'pietari.poutiainen@gmail.com'  # This is another valid field
-}
-
-@app.route('/graphdemo')
-def graphdemo():
-
-
-    labels, values = make_data_for_demograph()
-    return render_template("graphdemo.html", labels=labels[-20:], values=values[-20:])
+    items = fetch_data_for_entry2table()
+    return render_template('index.html', labels=labels[:], values=values[:], items=items[:5])
 
 def make_data_for_demograph() -> (list, list):
 
     sample_url = "{}/{}".format(Commons._OSRS_API, "graph/4151.json")
-    response = requests.get(sample_url).json()
-
+    response = Commons.session().get(sample_url).json()
     data = response['daily']
 
     labels = []
     values = []
-
-
     for (l, v) in data.items():
         timestamp = int(l) / 1000.0
         label = datetime.fromtimestamp(timestamp).strftime('%Y/%m/%d %H:%M:%s')
         labels.append(label)
         values.append(v)
     
-
     return labels, values
+
+def fetch_data_for_entry2table():
+
+    url: str = "{}/{}".format(Commons._OSRS_WIKI_API, "mapping")
+    response = Commons.session().get(url).json()
+
+    """
+    {
+        'examine': 'A fabulously ancient gnarled staff as used by the druids of old.', 
+        'id': 23342, 
+        'members': True, 
+        'lowalch': 80000, 
+        'limit': 8, 
+        'value': 200000, 
+        'highalch': 120000, 
+        'icon': '3rd age druidic staff.png', 
+        'name': '3rd age druidic staff'
+    }
+    """
+    return response
